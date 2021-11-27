d2a395a96db8b394a91e978237415f84d7c63c3b
Random Access Exomemory <Random.Access.Exomemory@gmail.com>
2021-11-27 19:18:12 +0000

    Minor update again to actually add a route that returns a HTMLResponse

--- web.py
+++ web.py
@@ -1,7 +1,10 @@
+from fastapi.responses import HTMLResponse
+import uvicorn
+
 from ext.web import app
 
 @app.get("/")
 def read_root():
-    return "Pong"
+    return HTMLResponse("Pong")
 
 uvicorn.run(app, host="0.0.0.0", port=8080, log_level="info")
